6c2a21129f774c75ac9ee68ee2dcdd57a0c9edc4
greyhoundforty <ryantiffany@fastmail.com>
2020-06-04 09:20:08 -0500

    first commit

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "zone1" {
+  default = "us-south-1"
+}
+
+variable "zone2" {
+  default = "us-south-2"
+}
+
+variable "ssh_public_key" {
+}
+
+variable "image" {
+  default = "99edcc54-c513-4d46-9f5b-36243a1e50e2"
+}
+
+variable "profile" {
+  default = "cx2-2x4"
+}
+
