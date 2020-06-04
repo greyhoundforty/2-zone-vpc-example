80a1cc949ab40c90d73a513d7628bad2d8211cc9
greyhoundforty <ryantiffany@fastmail.com>
2020-06-04 16:00:01 -0500

    Added tags for easier searching

--- main.tf
+++ main.tf
@@ -1,42 +1,43 @@
 resource "random_id" "vpc_name" {
-  byte_length = 4
+  byte_length = 2
 }
 
 resource "ibm_is_vpc" "vpc1" {
   name           = "vpc-${random_id.vpc_name.hex}"
   resource_group = data.ibm_resource_group.group.id
+  tags           = ["vpc-${random_id.vpc_name.hex}", ryantiffany]
 }
 
 resource "ibm_is_instance" "z1_instance1" {
-  name    = "z1-${random_id.vpc_name.hex}-1"
-  image   = var.image
-  profile = var.profile
+  vpc            = ibm_is_vpc.vpc1.id
+  zone           = var.zone1
+  resource_group = data.ibm_resource_group.group.id
+  name           = "z1-${random_id.vpc_name.hex}-1"
+  image          = var.image
+  profile        = var.profile
+  keys           = [data.ibm_is_ssh_key.vpc_us_south_key.id]
+  user_data      = file("nginx.sh")
+  tags           = ["vpc-${random_id.vpc_name.hex}", ryantiffany]
 
   primary_network_interface {
     subnet = ibm_is_subnet.z1_subnet1.id
   }
-
-  vpc            = ibm_is_vpc.vpc1.id
-  zone           = var.zone1
-  keys           = [data.ibm_is_ssh_key.vpc_us_south_key.id]
-  user_data      = file("nginx.sh")
-  resource_group = data.ibm_resource_group.group.id
 }
 
 resource "ibm_is_instance" "z2_instance1" {
-  name    = "z2-${random_id.vpc_name.hex}-1"
-  image   = var.image
-  profile = var.profile
-
-  primary_network_interface {
-    subnet = ibm_is_subnet.z2_subnet1.id
-  }
-
   vpc            = ibm_is_vpc.vpc1.id
   zone           = var.zone2
+  resource_group = data.ibm_resource_group.group.id
+  name           = "z2-${random_id.vpc_name.hex}-1"
+  image          = var.image
+  profile        = var.profile
   keys           = [data.ibm_is_ssh_key.vpc_us_south_key.id]
   user_data      = file("nginx.sh")
-  resource_group = data.ibm_resource_group.group.id
+  tags           = ["vpc-${random_id.vpc_name.hex}", ryantiffany]
+
+  primary_network_interface {
+    subnet = ibm_is_subnet.z1_subnet2.id
+  }
 }
 
 resource "ibm_is_volume" "z1_vol1" {
